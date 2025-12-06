11f009f0e4ed2a484748f0f1578ba4dae7678683
shengming hu <shmhu@hotmail.com>
2025-12-06 21:18:17 +1100

    chapter 8 - functions

---
+++ Chapter 8/city.py
@@ -0,0 +1,33 @@
+def city_country(name, country):
+    name_country = name + ', ' + country
+    return name_country
+
+print(city_country('Canberra', 'Australia'))
+
+def make_album(artist, album, tracks = ''):
+    music = {
+        'artist': '',
+        'album':''
+    }
+
+    music['artist'] = artist
+    music['album'] = album
+
+    if tracks:
+        music['tracks'] = tracks    
+    
+    return music
+
+information = make_album('abc', 'xyz')
+print(information)
+
+while True:
+    print("Enter q at any time to quit")
+    artist = input("Artist: ")
+    if artist == 'q':
+        break
+    album = input("Album: ")
+    if album == 'q':
+        break
+    information = make_album(artist, album)
+    print(information)
---
+++ Chapter 8/display.py
@@ -0,0 +1,9 @@
+def display_message():
+    print("We are learning about functions")
+
+display_message()
+
+def favorite_book(title):
+    print("One of my favorite books is", title)
+
+favorite_book('Alice in Wonderland')
