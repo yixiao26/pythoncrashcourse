93ef140c4fd3349a3fa4d17e3c96493fe19a18ae
shengming hu <shmhu@hotmail.com>
2025-12-16 21:36:58 +1100

    ending of chapter 9

--- Chapter 9/admin.py
+++ Chapter 9/admin.py
@@ -15,48 +15,4 @@ class User():
         print(f"Hello, {self.first_name} {self.last_name}!")
 
 
-class Privileges():
-    """A class to represent privileges of an admin user."""
-
-    def __init__(self, privileges=None):
-        """Initialize the privileges attribute."""
-        if privileges is None:
-            self.privileges = [
-                'can add post',
-                'can delete post',
-                'can ban user',
-                'can modify settings'
-            ]
-        else:
-            self.privileges = privileges
-
-    def show_privileges(self):
-        """Display the privileges."""
-        print("Admin has the following privileges:")
-        for privilege in self.privileges:
-            print(f"- {privilege}")
-
-class Admin(User):
-    """Represent a user with administrative privileges."""
-
-    def __init__(self, first_name, last_name):
-        """Initialize the admin user."""
-        super().__init__(first_name, last_name)
-        self.privileges = Privileges()
-
-    def show_privileges(self):
-        """Display the admin's privileges."""
-        self.privileges.show_privileges()
         
-admin = Admin('Alice', 'Smith')
-admin.show_privileges()
-admin.increment_login_attempts()
-admin.increment_login_attempts()
-admin.increment_login_attempts()
-admin.increment_login_attempts()
-admin.increment_login_attempts()
-admin.increment_login_attempts()
-print(f"Login attempts: {admin.login_attempts}")
-admin.reset_login_attempts()
-admin.privileges.show_privileges()
-admin.show_privileges()
---
+++ Chapter 9/my_admin.py
@@ -0,0 +1,5 @@
+from admin import User 
+from admin_pt2 import Privileges, Admin 
+
+admin = Admin('Alice', 'Smith')
+admin.show_privileges()
