10c61c0a0919187d4d2194fff03c75abcdede866
shengming hu <shmhu@hotmail.com>
2026-01-07 21:22:51 +1100

    added play button

--- Part 2/alien_invasion/alien_invasion.py
+++ Part 2/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -14,6 +15,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     ship = Ship(ai_settings, screen)
     # make a group to store bullets in.
@@ -27,7 +31,7 @@ def run_game():
     # Start the main loop for the game
     while True:
         # Watch for keyboard and mouse events
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
@@ -35,7 +39,7 @@ def run_game():
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
         # Draw all objects in game
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
--- Part 2/alien_invasion/game_functions.py
+++ Part 2/alien_invasion/game_functions.py
@@ -33,18 +33,53 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(
+                ai_settings,
+                screen,
+                stats,
+                play_button,
+                ship,
+                aliens,
+                bullets,
+                mouse_x,
+                mouse_y,
+            )
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(
+    ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y
+):
+    """Start a new game when the player clicks Play."""
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
+
+        # Reset the game statistics
+        stats.reset_stats()
+        stats.game_active = True
+
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+
+
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the
     screen.fill(ai_settings.bg_color)
@@ -54,6 +89,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the play button if the game is inactive.
+    if not stats.game_active:
+        play_button.draw_button()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
@@ -158,6 +197,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
