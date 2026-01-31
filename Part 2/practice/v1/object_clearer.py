16e8ee85ced97e8bf3336378b681f5972db06793
shengming hu <shmhu@hotmail.com>
2026-01-31 21:14:48 +1100

    update moved files from ages ago

--- Part 2/practice/v1/object_clearer.py
+++
@@ -1,4 +0,0 @@
-def clear_bullet(bullets):
-    for bullet in bullets.copy():
-        if bullet.rect.top < 0:
-            bullets.remove(bullet)
--- Part 2/practice/v1/utility.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from pathlib import Path
-def load_image(file_name):
-    file_folder = Path(__file__).resolve().parent
-    image_path = file_folder / f'images/{file_name}'
-    return pygame.image.load(image_path)
