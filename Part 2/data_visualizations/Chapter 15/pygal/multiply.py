6f6404fe7f5efd1f18c20a29fe5925019818a1fe
shengming hu <shmhu@hotmail.com>
2026-01-31 21:13:43 +1100

    finished histogram and die test

--- Part 2/data_visualizations/Chapter 15/pygal/multiply.py
+++ Part 2/data_visualizations/Chapter 15/pygal/multiply.py
@@ -4,18 +4,17 @@ from die import Die
 
 die_1 = Die()
 die_2 = Die()
-die_3 = Die()
 
 
 results = []
 
 for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll() + die_3.roll()
+    result = die_1.roll() * die_2.roll()
     results.append(result)
 
 
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+max_result = die_1.num_sides * die_2.num_sides
 
 for value in range(1, max_result + 1):
     frequency = results.count(value)
@@ -30,4 +29,4 @@ hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
 hist.add("D6 + D10", frequencies)
-hist.render_to_file("die_visual.svg")
+hist.render_to_file("sum_visuals.svg")
